348e264994bb991926c6dc67c8adfa3d7a44faf1
satooru65536 <ryuto4619@gmail.com>
2024-10-12 11:19:36 +0900

    update: 注文

--- src/const/items.ts
+++ src/const/items.ts
@@ -4,28 +4,43 @@ export const itemGroup = {
   food: [
     {
       name: '焼き鳥もも(塩こしょう)',
-      price: 150,
+      price: 120,
       image: '/sample.png',
     },
     {
       name: '焼き鳥ねぎま(塩こしょう)',
-      price: 150,
+      price: 120,
       image: '/sample.png',
     },
     {
       name: '焼き鳥もも(タレ)',
-      price: 150,
+      price: 120,
       image: '/sample.png',
     },
     {
       name: '焼き鳥ねぎま(タレ)',
-      price: 150,
+      price: 120,
+      image: '/sample.png',
+    },
+    {
+      name: '焼き鳥4本セット(各種1本)',
+      price: 400,
+      image: '/sample.png',
+    },
+    {
+      name: '焼き鳥24本セット(各種6本)',
+      price: 2000,
       image: '/sample.png',
     },
   ],
   drink: [
     {
-      name: 'コーラ',
+      name: 'コカコーラ',
+      price: 100,
+      image: '/sample.png',
+    },
+    {
+      name: 'ドクタペッパー',
       price: 100,
       image: '/sample.png',
     },
@@ -34,7 +49,18 @@ export const itemGroup = {
       price: 100,
       image: '/sample.png',
     },
+    {
+      name: 'ファンタグレープ',
+      price: 100,
+      image: '/sample.png',
+    },
+    {
+      name: '緑茶',
+      price: 100,
+      image: '/sample.png',
+    },
   ],
 } as const satisfies ItemGroup;
 
 export const itemList = Object.values(itemGroup).flat();
+ï
